8f12d53c9aee50e9e34668125a961ed9c98521f2
1102-Tyler-Johnson <tylerjjohnson@nevada.unr.edu>
2024-04-29 17:28:55 +0000

    1

--- imagedesign.c
+++ imagedesign.c
@@ -1,1 +1,26 @@
-Hello World!
+// Tyler Johnson
+// Ayla Velasquez 
+
+#include <stdio.h> 
+
+int main (){ 
+	
+
+
+
+return 0;
+} 
+
+void GetImage(){ 
+	FILE *imagefp;
+	imagefp = fopen ("image.txt", "w"); 
+	
+	if (imagefp == NULL){ 
+		printf("Can not open file\n");
+	}
+	else{ 
+		
+	
+	
+	}
+}

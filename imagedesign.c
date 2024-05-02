65b0215e9e64d4c8cf4042ba9dfc3417c388158b
1102-Velasquez-Ayla <aylavely@gmail.com>
2024-05-02 17:06:57 +0000

    1st menu done

--- imagedesign.c
+++ imagedesign.c
@@ -3,6 +3,8 @@
 
 #include <stdio.h> 
 
+int Menu();
+
 int main (){ 
 	
 
@@ -10,6 +12,21 @@ int main (){
 
 return 0;
 } 
+
+int Menu(){
+	int choice;
+
+	printf("**ERINSTAGRAM**\n");
+	printf("1: Load image\n");
+	printf("2: Display image\n");
+	printf("3: Edit image\n");
+	printf("0: Exit\n");
+	
+	printf("\nChoose from one of the options above:");
+	scanf("%d", &choice);
+	
+	return choice;
+}
 
 void GetImage(){ 
 	FILE *imagefp;
